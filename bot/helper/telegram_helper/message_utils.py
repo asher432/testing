9b089efa8bcb8a53c65928b66b7069e1accf0f6c
sachin-sar <107174628+sachin-sar@users.noreply.github.com>
2022-06-20 22:09:00 +0530

    Update message_utils.py

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -6,7 +6,7 @@ import time
 from time import sleep
 import psutil, shutil
 from bot import botStartTime,DOWNLOAD_STATUS_UPDATE_INTERVAL, dispatcher, OWNER_ID, AUTO_DELETE_MESSAGE_DURATION, LOGGER, bot, \
-    status_reply_dict, status_reply_dict_lock, download_dict, download_dict_lock, Interval
+    status_reply_dict, status_reply_dict_lock, download_dict, download_dict_lock, Interval, STATUS_LIMIT 
 from bot.helper.ext_utils.bot_utils import get_readable_message, get_readable_file_size, get_readable_time, progress_bar, MirrorStatus, setInterval
 from telegram.error import TimedOut, BadRequest, RetryAfter
 from pyrogram.errors import FloodWait
