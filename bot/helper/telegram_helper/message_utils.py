9ae26d335cd1d4d39af54b6cc0b86ee9f792824d
sachin-sar <107174628+sachin-sar@users.noreply.github.com>
2022-06-21 00:59:12 +0530

    Update message_utils.py

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -10,7 +10,10 @@ from html import escape
 from urllib.request import urlopen
 import time
 from time import sleep
-import psutil, shutil
+import psutil
+import shutil
+from psutil import *
+from shutil import *
 from bot import botStartTime,DOWNLOAD_STATUS_UPDATE_INTERVAL, dispatcher, OWNER_ID, AUTO_DELETE_MESSAGE_DURATION, LOGGER, bot, \
     status_reply_dict, status_reply_dict_lock, download_dict, download_dict_lock, Interval, STATUS_LIMIT, DOWNLOAD_DIR
 from bot.helper.ext_utils.bot_utils import get_readable_message, get_readable_file_size, get_readable_time, progress_bar,get_progress_bar_string, MirrorStatus, setInterval, \
