8e29346c0aaf731e04d40a0e4a1c8e3ddb52a19e
sachin-sar <107174628+sachin-sar@users.noreply.github.com>
2022-06-21 01:44:20 +0530

    Update message_utils.py

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -239,9 +239,9 @@ def get_readable_message():
         bmsg += f"\n<b>DL:</b> {get_readable_file_size(dlspeed_bytes)}/s | <b>UL:</b> {get_readable_file_size(upspeed_bytes)}/s"
         
         buttons = ButtonMaker()
-        buttons.sbutton("Refresh", str(ONE))
-        buttons.sbutton("Close", str(TWO))
-        buttons.sbutton("Statistics", str(THREE))
+        buttons.sbutton("Refresh", callback_data=str(ONE))
+        buttons.sbutton("Close", callback_data=str(TWO))
+        buttons.sbutton("Statistics", callback_data=str(THREE))
         sbutton = InlineKeyboardMarkup(buttons.build_menu(3))
         
         if STATUS_LIMIT is not None and tasks > STATUS_LIMIT:
