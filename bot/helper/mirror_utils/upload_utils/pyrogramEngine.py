655050bdeaf29f8268ef834e1289704edf5006d9
sachin-sar <107174628+sachin-sar@users.noreply.github.com>
2022-06-20 15:44:51 +0530

    Update pyrogramEngine.py

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -27,7 +27,7 @@ class TgUploader:
         self.uploaded_bytes = 0
         self._last_uploaded = 0
         self.__listener = listener
-        self.__start_time = time()
+        self.__start_time = time.time()
         self.__total_files = 0
         self.__is_cancelled = False
         self.__as_doc = AS_DOCUMENT
@@ -208,7 +208,7 @@ class TgUploader:
     def speed(self):
         with self.__resource_lock:
             try:
-                return self.uploaded_bytes / (time() - self.__start_time)
+                return self.uploaded_bytes / (time.time() - self.__start_time)
             except ZeroDivisionError:
                 return 0
 
