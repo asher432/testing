240bb25b26e9b84aa9ec4c0d14b8b9c0332eea09
sachin-sar <107174628+sachin-sar@users.noreply.github.com>
2022-06-21 10:58:51 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -296,6 +296,28 @@ def get_readable_message():
             return msg + bmsg, button
         return msg + bmsg, sbutton        
 
+def turn(data):
+    try:
+        with download_dict_lock:
+            global COUNT, PAGE_NO
+            if data[1] == "nex":
+                if PAGE_NO == pages:
+                    COUNT = 0
+                    PAGE_NO = 1
+                else:
+                    COUNT += STATUS_LIMIT
+                    PAGE_NO += 1
+            elif data[1] == "pre":
+                if PAGE_NO == 1:
+                    COUNT = STATUS_LIMIT * (pages - 1)
+                    PAGE_NO = pages
+                else:
+                    COUNT -= STATUS_LIMIT
+                    PAGE_NO -= 1
+        return True
+    except:
+        return False
+    
 ONE, TWO, THREE = range(3)
 
 def refresh(update, context):
