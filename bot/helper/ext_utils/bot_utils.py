7bb0de9922fb938a329905a4b0e482ba767f89b2
sachin-sar <107174628+sachin-sar@users.noreply.github.com>
2022-06-21 11:05:26 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -30,7 +30,7 @@ URL_REGEX = r"(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+"
 COUNT = 0
 PAGE_NO = 1
 
-INISHED_PROGRESS_STR = "▓"
+FINISHED_PROGRESS_STR = "▓"
 UNFINISHED_PROGRESS_STR = "░"
 PROGRESS_MAX_SIZE = 100 // 8
 
