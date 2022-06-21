3de19da77b8c09f25a3fc3c423636570ae394373
sachin-sar <107174628+sachin-sar@users.noreply.github.com>
2022-06-21 15:09:36 +0530

    Update message_utils.py

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -120,13 +120,13 @@ def delete_all_messages():
 
 def update_all_messages():
     msg, buttons = get_readable_message()
-    msg += f"<b>▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬</b>\n\n"
     with status_reply_dict_lock:
         for chat_id in list(status_reply_dict.keys()):
             if status_reply_dict[chat_id] and msg != status_reply_dict[chat_id].text:
                 if buttons == "":
                     editMessage(msg, status_reply_dict[chat_id])
                 else:
+                    msg += f"\n\n<b>▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬</b>\n\n"
                     editMessage(msg, status_reply_dict[chat_id], buttons)
                 status_reply_dict[chat_id].text = msg
 
