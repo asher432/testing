b42eec7efc84960c2566762884a37b4eeb6912b1
archon0p <107740668+archon0p@users.noreply.github.com>
2022-06-25 12:24:50 +0530

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -592,11 +592,8 @@ def udrive(url: str) -> str:
         gd_id = res.rsplit("=", 1)[-1]
         flink = f"https://drive.google.com/open?id={gd_id}"
         return flink
-    try:
-      res = client.post(req_url, headers=headers, data=data).json()["file"]
-      gd_id = re_findall('gd=(.*)', res, re.DOTALL)[0]
-    except:
-      raise DirectDownloadLinkException("ERROR: Try in your broswer, mostly file not found or user limit exceeded!")
+   else:
+        gd_id = re_findall('gd=(.*)', res, re.DOTALL)[0]
         
     info_parsed['gdrive_url'] = f"https://drive.google.com/open?id={gd_id}"
     info_parsed['src_url'] = url
